9a1f4156dd97e5b1ecbfaf65385d71d2fb77841e
dbicanic <dbicanic77@gmail.com>
2016-09-01 15:38:51 -0500

    Have a map that displays direction, but doesn't give turn by turn, may need to look into something else

--- app/assets/javascripts/trucks.js
+++ app/assets/javascripts/trucks.js
@@ -5,12 +5,45 @@ $( document ).ready(function() {
 	});
 });
 
-$(document).ready(function()) {
-	function initMap() {
-	  var mapDiv = document.getElementById('map');
-	  var map = new google.maps.Map(mapDiv, {
-	      center: {lat: 44.540, lng: -78.546},
-	      zoom: 8
-	  });
-	}
-}
+// function initMap() {
+//   var mapDiv = document.getElementById('map');
+//   var map = new google.maps.Map(mapDiv, {
+//       center: {lat: 42.296704, lng: -87.876429},
+//       zoom: 8
+//   });
+// }
+
+if (navigator.geolocation) { //Checks if browser supports geolocation
+   navigator.geolocation.getCurrentPosition(function (position) {                                                              //This gets the
+     var latitude = position.coords.latitude;                    //users current
+     var longitude = position.coords.longitude;                 //location
+     var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
+     var directionsService = new google.maps.DirectionsService();
+     var directionsDisplay = new google.maps.DirectionsRenderer();
+     var mapOptions = //Sets map options
+     {
+       zoom: 15,  //Sets zoom level (0-21)
+       center: coords, //zoom in on users location
+       mapTypeControl: true, //allows you to select map type eg. map or satellite
+       navigationControlOptions:
+       {
+         style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
+       },
+       mapTypeId: google.maps.MapTypeId.ROADMAP //sets type of map Options:ROADMAP, SATELLITE, HYBRID, TERRIAN
+     };
+     map = new google.maps.Map( /*creates Map variable*/ document.getElementById("map"), mapOptions /*Creates a new map using the passed optional parameters in the mapOptions parameter.*/);
+     directionsDisplay.setMap(map);
+     directionsDisplay.setPanel(document.getElementById('panel'));
+     var request = {
+       origin: coords,
+       destination: {lat: 42.296704, lng: -87.876429},
+       travelMode: google.maps.DirectionsTravelMode.DRIVING
+     };
+
+     directionsService.route(request, function (response, status) {
+       if (status == google.maps.DirectionsStatus.OK) {
+         directionsDisplay.setDirections(response);
+       }
+     });
+   });
+ }
