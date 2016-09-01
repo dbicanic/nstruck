7beed4b24206930f8ed385d4424053f658362f45
dbicanic <dbicanic77@gmail.com>
2016-09-01 09:59:11 -0500

    Starting on maps

--- app/assets/javascripts/trucks.js
+++ app/assets/javascripts/trucks.js
@@ -4,3 +4,13 @@ $( document ).ready(function() {
 	    $("#wrapper").toggleClass("toggled");
 	});
 });
+
+$(document).ready(function()) {
+	function initMap() {
+	  var mapDiv = document.getElementById('map');
+	  var map = new google.maps.Map(mapDiv, {
+	      center: {lat: 44.540, lng: -78.546},
+	      zoom: 8
+	  });
+	}
+}
