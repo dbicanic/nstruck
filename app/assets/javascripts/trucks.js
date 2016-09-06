afea95fc59a7d8d2bd3b5085c5fec26e1a856fc8
dbicanic <dbicanic77@gmail.com>
2016-09-06 10:52:33 -0500

    Add in map, link to directions.

--- app/assets/javascripts/trucks.js
+++ app/assets/javascripts/trucks.js
@@ -5,45 +5,25 @@ $( document ).ready(function() {
 	});
 });
 
-// function initMap() {
-//   var mapDiv = document.getElementById('map');
-//   var map = new google.maps.Map(mapDiv, {
-//       center: {lat: 42.296704, lng: -87.876429},
-//       zoom: 8
-//   });
-// }
+var directionsDisplay;
+var map;
 
-if (navigator.geolocation) { //Checks if browser supports geolocation
-   navigator.geolocation.getCurrentPosition(function (position) {                                                              //This gets the
-     var latitude = position.coords.latitude;                    //users current
-     var longitude = position.coords.longitude;                 //location
-     var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
-     var directionsService = new google.maps.DirectionsService();
-     var directionsDisplay = new google.maps.DirectionsRenderer();
-     var mapOptions = //Sets map options
-     {
-       zoom: 15,  //Sets zoom level (0-21)
-       center: coords, //zoom in on users location
-       mapTypeControl: true, //allows you to select map type eg. map or satellite
-       navigationControlOptions:
-       {
-         style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
-       },
-       mapTypeId: google.maps.MapTypeId.ROADMAP //sets type of map Options:ROADMAP, SATELLITE, HYBRID, TERRIAN
-     };
-     map = new google.maps.Map( /*creates Map variable*/ document.getElementById("map"), mapOptions /*Creates a new map using the passed optional parameters in the mapOptions parameter.*/);
-     directionsDisplay.setMap(map);
-     directionsDisplay.setPanel(document.getElementById('panel'));
-     var request = {
-       origin: coords,
-       destination: {lat: 42.296704, lng: -87.876429},
-       travelMode: google.maps.DirectionsTravelMode.DRIVING
-     };
+//Initiates the map, using Northshore truck as the main focus point.
+function initMap() {
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	var nstruck = new google.maps.LatLng(42.296704, -87.876429);
 
-     directionsService.route(request, function (response, status) {
-       if (status == google.maps.DirectionsStatus.OK) {
-         directionsDisplay.setDirections(response);
-       }
-     });
-   });
- }
+	var mapOptions = {
+		zoom: 14,
+		center: nstruck
+	}
+
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	directionsDisplay.setMap(map);
+
+	var marker = new google.maps.Marker({
+		position: nstruck,
+		map: map,
+		title: 'Northshore Truck & Equipment Co.'
+	});
+}
